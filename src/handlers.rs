82be0a766c751db2947ba86a2939f610c9b17b05
gpi <gpimpale29@gmail.com>
2022-12-30 10:23:15 -0800

    stuff

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,13 @@
+use std::{time::{Duration, Instant}};
+
 use super::AppData;
-use actix_web::{http::StatusCode, web, HttpResponse, Responder, ResponseError};
+use actix_web::{http::StatusCode, web, HttpResponse, Responder, ResponseError, Error, HttpRequest};
+use actix_ws::Message;
 use derive_more::Display;
+use futures_util::{future::{self, Either}, StreamExt};
 use serde::{Deserialize, Serialize};
 use todoproxy_api::response::Info;
+use tokio::time::interval;
 
 #[derive(Clone, Debug, Serialize, Deserialize, Display)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
@@ -56,11 +61,105 @@ pub async fn info() -> Result<impl Responder, AppError> {
 
 // start websocket connection
 #[actix_web::get("/ws")]
-pub async fn ws() -> Result<impl Responder, AppError> {
-    return Ok(web::Json(Info {
-        service: String::from(super::SERVICE),
-        version_major: super::VERSION_MAJOR,
-        version_minor: super::VERSION_MINOR,
-        version_rev: super::VERSION_REV,
-    }));
+pub async fn ws(req: HttpRequest, stream: web::Payload) -> Result<impl Responder, Error> {
+    let (res, session, msg_stream) = actix_ws::handle(&req, stream)?;
+    // spawn websocket handler (and don't await it) so that the response is returned immediately
+    tokio::spawn(handler::echo_ws(session, msg_stream));
+    Ok(res)
+}
+
+
+/// How often heartbeat pings are sent.
+///
+/// Should be half (or less) of the acceptable client timeout.
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+
+/// How long before lack of client response causes a timeout.
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+/// Echo text & binary messages received from the client, respond to ping messages, and monitor
+/// connection health to detect network issues and free up resources.
+pub async fn echo_heartbeat_ws(
+    mut session: actix_ws::Session,
+    mut msg_stream: actix_ws::MessageStream,
+) {
+    log::info!("connected");
+
+    let mut last_heartbeat = Instant::now();
+    let mut interval = interval(HEARTBEAT_INTERVAL);
+
+    let reason = loop {
+        // create "next client timeout check" future
+        let tick = interval.tick();
+        // required for select()
+        tokio::pin!(tick);
+
+        // waits for either `msg_stream` to receive a message from the client or the heartbeat
+        // interval timer to tick, yielding the value of whichever one is ready first
+        match future::select(msg_stream.next(), tick).await {
+            // received message from WebSocket client
+            Either::Left((Some(Ok(msg)), _)) => {
+                log::debug!("msg: {msg:?}");
+
+                match msg {
+                    Message::Text(text) => {
+                        session.text(text).await.unwrap();
+                    }
+
+                    Message::Binary(bin) => {
+                        session.binary(bin).await.unwrap();
+                    }
+
+                    Message::Close(reason) => {
+                        break reason;
+                    }
+
+                    Message::Ping(bytes) => {
+                        last_heartbeat = Instant::now();
+                        let _ = session.pong(&bytes).await;
+                    }
+
+                    Message::Pong(_) => {
+                        last_heartbeat = Instant::now();
+                    }
+
+                    Message::Continuation(_) => {
+                        log::warn!("no support for continuation frames");
+                    }
+
+                    // no-op; ignore
+                    Message::Nop => {}
+                };
+            }
+
+            // client WebSocket stream error
+            Either::Left((Some(Err(err)), _)) => {
+                log::error!("{}", err);
+                break None;
+            }
+
+            // client WebSocket stream ended
+            Either::Left((None, _)) => break None,
+
+            // heartbeat interval ticked
+            Either::Right((_inst, _)) => {
+                // if no heartbeat ping/pong received recently, close the connection
+                if Instant::now().duration_since(last_heartbeat) > CLIENT_TIMEOUT {
+                    log::info!(
+                        "client has not sent heartbeat in over {CLIENT_TIMEOUT:?}; disconnecting"
+                    );
+
+                    break None;
+                }
+
+                // send heartbeat ping
+                let _ = session.ping(b"").await;
+            }
+        }
+    };
+
+    // attempt to close connection gracefully
+    let _ = session.close(reason).await;
+
+    log::info!("disconnected");
 }
